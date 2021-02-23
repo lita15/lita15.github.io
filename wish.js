a98a55708cf242a0b59ec1cdc26303e9f643596b
lita15 <72492009+lita15@users.noreply.github.com>
2021-02-23 22:19:06 +0700

    Add files via upload

---
+++ wish.js
@@ -0,0 +1,15 @@
+var b = prompt("Masukin passwordnya dulu (tanggal jadian, hayoo inget ingett)");
+if (b == "27") {
+    var name = prompt("Ayo masukin namanaya");
+    alert("Halo " + name + ". Gemeshnya lita ya ini hehehe");
+    var confirm = confirm("Ishhh " + name + ", gemeshnya lita bukan si? :(");
+    if (confirm === true) {
+        alert("Emmm litanya mau bilang sesuatu niii. Agak panjang dikit tapi, dibaca ya");
+        alert("Hallo, happy birthday byy. Sehat terus ya sayangg, semoga yang dipengenin cepet ke kabul semua. Makin tahun makin produktiv, yang di cita-cita in cepet kesampaian. Semuanya dibuat lancar sama Allah. Wish u all the best. Makasih ya udah mau nrima aku apa adanya. Sayang maaf ya, ultah mu aku gak bisa kesana. Cuma bisa ngirim ini doang, semoga engga cringe hehe. Sayangg byy bangett, mmmuuahhh. Sekali lagi Selamat Ulang Tahun ke 19 byy <3");
+        alert("Love u more and more");
+    } else {
+        alert("Gak tau aku ngambekk");
+    }
+} else {
+    alert("Hayolohh lupaa, dahlahh");
+}
